b6c831caae5c872f4b8f35dca0f81a624691df0b
Ande Caleb <caleb.ande@fintraksoftware.com>
2019-12-19 05:52:13 -0800

    Sterling Pi360 updated files

---
+++ Presentation/Fintrak.Presentation.WebClient/Controllers/MVC/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using Fintrak.Presentation.WebClient.Core;
+using Fintrak.Presentation.WebClient.Models;
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Web.Mvc;
+using System.Configuration;
+using WebMatrix.WebData;
+//using System.Web.Configuration;
+
+namespace Fintrak.Presentation.WebClient.Controllers
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [RoutePrefix("account")]
+    public class AccountController : ViewControllerBase
+    {
+        [ImportingConstructor]
+        public AccountController(ISecurityAdapter securityAdapter)
+        {
+
+            _SecurityAdapter = securityAdapter;
+        }
+
+        ISecurityAdapter _SecurityAdapter;
+
+        //public string _status = WebConfigurationManager.AppSettings["Status"];
+
+
+        [HttpGet]
+        // the route is defined in RouteConfig
+        public ActionResult Register()
+        {
+            _SecurityAdapter.Initialize();
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public ActionResult Login(string returnUrl)
+        {
+            _SecurityAdapter.Initialize();
+
+            string configReferrer = ConfigurationManager.AppSettings["expectedreferal"];
+            string defaultReferrer = ConfigurationManager.AppSettings["defaultReferrer"];
+            string actualreferrer = string.Empty;
+
+            if (HttpContext.Request.UrlReferrer != null)
+            {
+                actualreferrer = HttpContext.Request.UrlReferrer.ToString();
+            }
+            else
+            {
+                actualreferrer = "";
+            }
+
+            if (actualreferrer == "" || actualreferrer == "null")
+            {
+                actualreferrer = "null";
+            }
+            if (actualreferrer == configReferrer)
+            {
+                return View(new AccountLoginModel() { ReturnUrl = returnUrl });
+            }
+            else
+            {
+                //Response.Redirect(defaultReferrer);
+                //return null;
+
+                _SecurityAdapter.Initialize();
+                _SecurityAdapter.LogOut();
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("logout")]
+        public ActionResult Logout()
+        {
+
+            _SecurityAdapter.Initialize();
+            //WebSecurity.Logout();
+            _SecurityAdapter.LogOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("changepassword")]
+        [Authorize]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("forgotpassword")]
+        [Authorize]
+        public ActionResult ForgotPassword()
+        {
+            _SecurityAdapter.Initialize();
+            return View();
+        }
+
+
+    }
+}
---
+++ Presentation/Fintrak.Presentation.WebClient/app/finstat/controllers/calendar-edit-controller.js
@@ -0,0 +1,136 @@
+/**
+ * Created by Deb on 8/20/2014.
+ */
+(function () {
+    "use strict";
+    angular
+        .module("fintrak")
+        .controller("CalendarEditController",
+                    ['$scope', '$window', '$state', '$stateParams', 'viewModelHelper', 'validator',
+                        CalendarEditController]);
+
+    function CalendarEditController($scope,$window, $state, $stateParams, viewModelHelper, validator) {
+        var vm = this;
+        vm.viewModelHelper = viewModelHelper;
+        vm.parentController = $scope.$parent;
+
+        vm.module = 'Finstat';
+        vm.view = 'calendar-edit-view';
+        vm.viewName = 'Calendar';
+        vm.status = false;
+        vm.openedRunDate = false;
+        vm.viewModelHelper.modelIsValid = true;
+        vm.viewModelHelper.modelErrors = [];
+
+        vm.calendars = {};
+        vm.calendarsobj = {};
+        vm.init = false;
+        vm.showInstruction = false;
+        vm.instruction = '';
+
+        var calendarRules = [];
+
+        var setupRules = function () {
+
+            calendarRules.push(new validator.PropertyRule("ThisDate", {
+                required: { message: "Date is required" }
+            }));
+
+            calendarRules.push(new validator.PropertyRule("FullDescription", {
+                required: { message: "FullDescription is required" }
+            }));
+
+        }
+
+        var initialize = function () {
+            if (vm.init === false) {
+                //load lookups
+                intializeLookUp();
+
+                if ($stateParams.CalId !== 0) {
+                    vm.showChildren = true;
+                    vm.viewModelHelper.apiGet('api/calendar/getcalendar/' + $stateParams.CalId, null,
+                   function (result) {
+                       vm.calendars = result.data;
+                       initialView();
+                       vm.init === true;
+                       
+                   },
+                   function (result) {
+                       toastr.error(result.data, 'Fintrak');
+                   }, null);
+                }
+                else
+                    vm.calendars = {
+                        //ThisDate: new date(),
+                        FullDEscription: '',                       
+                        Active: true
+                    };
+            }
+        }
+
+        var intializeLookUp = function () {
+           
+        }
+
+        var initialView = function () {
+            
+        }
+
+        vm.save = function () {
+            //Validate
+            validator.ValidateModel(vm.calendars, calendarRules);
+            vm.viewModelHelper.modelIsValid = vm.calendars.isValid;
+            vm.viewModelHelper.modelErrors = vm.calendars.errors;
+            if (vm.viewModelHelper.modelIsValid) {        
+                vm.viewModelHelper.apiPost('api/calendar/updatecalendar', vm.calendars,
+               function (result) {                   
+                   $state.go('finstat-calendar-list');
+               },
+               function (result) {
+                   toastr.error(result.data, 'Fintrak');
+               }, null);
+            }
+            else
+            {
+                vm.viewModelHelper.modelErrors = vm.calendars.errors;
+
+                var errorList = '';
+
+                angular.forEach(vm.viewModelHelper.modelErrors, function (error) {
+                    errorList += error + '<br>';
+                });
+                toastr.error(errorList, 'Fintrak');
+            }
+                
+        }
+
+        vm.delete = function () {
+            var deleteFlag = $window.confirm(' Are you sure you want to delete' );
+
+            if (deleteFlag) {
+                vm.viewModelHelper.apiPost('api/calendar/deletecalendar', vm.calendars.CalId,
+              function (result) {
+                  toastr.success('Selected item deleted.', 'Fintrak');
+                  $state.go('finstat-calendar-list');
+              },
+              function (result) {
+                  toastr.error(result.data, 'Fintrak');
+              }, null);
+            } 
+        }
+
+        vm.cancel = function () {
+            $state.go('finstat-calendar-list');
+        };
+
+        vm.openRunDate = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            vm.openedRunDate = true;
+        }
+        setupRules();
+        initialize();
+
+    }
+}());
